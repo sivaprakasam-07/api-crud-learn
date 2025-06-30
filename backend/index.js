97cbdbefd830fc7f524eedf097909121c9041ede
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-06-30 23:54:02 +0530

    created a cluster and then connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express=require('express');
+const mongoose=require('mongoose');
 const app=express();
 app.listen(3000,()=>{
     console.log("Sivaprakasam T")
 });
+mongoose.connect('mongodb+srv://tamilmanisivaprakasam5:LEGEND%402005@cluster07.iowpaeh.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster07')
+  .then(() => {console.log('Connected!')})
+  .catch(()=>{
+    console.log("Connection failed");
+  });
 app.get('/',(req,res)=>{
     res.send("Welcome Sivaprakasam")
 });
